8dbb19449a7a6cd89cc70f019cd375666b6a3217
jake-xy <fxyrus@gmail.com>
2026-03-19 10:16:30 +0800

    added gitignore

--- obj/Debug/net10.0/LearnerProfile.app.AssemblyInfo.cs
+++ obj/Debug/net10.0/LearnerProfile.app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LearnerProfile.app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56f1acebd5c69ec1a7e169459c14430d5b59f4a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LearnerProfile.app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LearnerProfile.app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
